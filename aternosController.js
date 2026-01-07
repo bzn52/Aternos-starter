6717a5671e7f9083855b6ee81e6abdb0d7da539d
Bizen <117585845+bzn52@users.noreply.github.com>
2026-01-07 20:06:26 +0545

    Update aternosController.js

--- aternosController.js
+++ aternosController.js
@@ -17,8 +17,6 @@ class AternosController {
   async initialize() {
     this.browser = await puppeteer.launch({
       headless: true,
-      executablePath:
-        process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
